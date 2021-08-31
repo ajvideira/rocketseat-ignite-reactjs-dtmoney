c6e385325fac7740d0c7b0e39d87aad21c3c45e3
Jonathan Alba Videira <jonathan.videira@gmail.com>
2021-08-31 07:27:24 -0300

    Creating modal NewTransactionModal structure and styling global modal css

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,4 +1,5 @@
 import ReactModal from "react-modal";
+import { Container } from "./styles";
 
 type NewTransactionModalProps = {
   isOpen: boolean;
@@ -10,8 +11,20 @@ export function NewTransactionModal({
   onRequestClose,
 }: NewTransactionModalProps) {
   return (
-    <ReactModal isOpen={isOpen} onRequestClose={onRequestClose}>
-      <h1>New Transaction</h1>
+    <ReactModal
+      isOpen={isOpen}
+      onRequestClose={onRequestClose}
+      overlayClassName="react-modal-overlay"
+      className="react-modal-content"
+    >
+      <Container>
+        <h1>New Transaction</h1>
+
+        <input type="text" placeholder="Título" />
+        <input type="text" placeholder="Valor" />
+        <input type="text" placeholder="Categoria" />
+        <input type="submit" value="Cadastrar" />
+      </Container>
     </ReactModal>
   );
 }
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -51,4 +51,25 @@ export const GlobalStyle = createGlobalStyle`
   [disabled] {
     cursor: not-allowed;
   }
+
+  .react-modal-overlay {
+    background: rgba(0, 0, 0, 0.5);
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    right: 0;
+    left: 0;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .react-modal-content {
+    background: var(--background);
+    width: 100%;
+    max-width: 576px;
+    border-radius: 0.25rem;
+    padding: 3rem;
+    position: relative;
+  }
 `;
